a098f992505e8fc4b6dd4bb923b377c674e200df
loulou8011984D <louptaxi@gmail.com>
2022-05-17 16:41:50 +0200

    encore

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-function valider() {
-  
-	var x = document.getElementById("in").value;
-	var entree = parseInt(x.replace(/-/g,""));
-	
-	var y = document.getElementById("out").value;
-	var sortie = parseInt(y.replace(/-/g,""));
-	
-	if(entree > sortie){
-		document.write("Veuillez entrer une date de départ ultérieure à la date d'arrivée");
-	}else if(entree < sortie){
-		document.write("Bingo !");
-	}
-	var x = document.getElementsById("valid").value;
-  if(x==""){
-    document.write("Veullez remplir les cases");
-  }
-}
+$('button').click(function(e) {
+	e.preventDefault();
+	$('main').addClass('flip');
+  });
