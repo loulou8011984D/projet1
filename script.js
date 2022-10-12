afbf606ecf01ed5b5d650d375b5942d19218fe37
loulou8011984D <louptaxi@gmail.com>
2022-10-12 19:01:50 +0200

    merge

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-$('button').click(function(e) {
-	e.preventDefault();
-	$('main').addClass('flip');
-  });
+const sendBtn = document.querySelector(".send-button");
+const container = document.querySelector(".container-notification");
+const form = document.querySelector(".form-horizontal");
+const title = document.querySelector(".section-header");
+console.log(container);
+
+form.onsubmit = function(event) {
+	event.preventDefault();
+};
+
+sendBtn.addEventListener("click", function(){
+	const notification = document.createElement("div");
+    notification.classList.add("notif");
+	notification.innerText = "Votre commande a été prise en compte";
+	title.innerText = "Commande prise en compte"
+	container.appendChild(notification);
+	setTimeout(function(){
+		notification.remove();
+}, 3000);
+})
+
